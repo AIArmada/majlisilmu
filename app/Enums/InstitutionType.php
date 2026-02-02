90a79112184bd18105a0f9c83a11e8519388a03b
Saiffil Fariz <saiffil@masyuk.com>
2026-02-02 19:46:12 +0800

    wip

--- app/Enums/InstitutionType.php
+++ app/Enums/InstitutionType.php
@@ -8,18 +8,41 @@ enum InstitutionType: string implements HasLabel
 {
     case Masjid = 'masjid';
     case Surau = 'surau';
-    case EducationalCenter = 'educational_center';
-    case CommunityCenter = 'community_center';
-    case Others = 'others';
+    case Madrasah = 'madrasah';
+    case Maahad = 'maahad';
+    case Pondok = 'pondok';
+    case Sekolah = 'sekolah';
+    case Kolej = 'kolej';
+    case Universiti = 'universiti';
+    case Pertubuhan = 'pertubuhan';
+    case Yayasan = 'yayasan';
+    case Persatuan = 'persatuan';
+    case Kelab = 'kelab';
+    case Usrah = 'usrah';
+    case Perniagaan = 'perniagaan';
+    case Syarikat = 'syarikat';
+    case Koperasi = 'koperasi';
+    case Hotel = 'hotel';
 
     public function getLabel(): string
     {
         return match ($this) {
             self::Masjid => __('Masjid'),
             self::Surau => __('Surau'),
-            self::EducationalCenter => __('Educational Center'),
-            self::CommunityCenter => __('Community Center'),
-            self::Others => __('Others'),
+            self::Madrasah => __('Madrasah'),
+            self::Maahad => __('Maahad'),
+            self::Pondok => __('Pondok'),
+            self::SekolahAgama => __('Sekolah Agama'),
+            self::KolejIslam => __('Kolej Islam'),
+            self::Universiti => __('Universiti'),
+            self::PertubuhanIslam => __('Pertubuhan Islam (NGO)'),
+            self::Yayasan => __('Yayasan'),
+            self::Persatuan => __('Persatuan'),
+            self::Kelab => __('Kelab'),
+            self::Usrah => __('Usrah'),
+            self::Perniagaan => __('Perniagaan'),
+            self::Syarikat => __('Syarikat'),
+            self::Koperasi => __('Koperasi'),
         };
     }
 }
