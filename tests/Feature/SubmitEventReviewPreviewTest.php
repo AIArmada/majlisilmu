6bcc1a1583542740b05d35c5185e4ba2b2016eaf
Saiffil Fariz <saiffil@masyuk.com>
2026-04-08 10:18:17 +0800

    wip

--- tests/Feature/SubmitEventReviewPreviewTest.php
+++ tests/Feature/SubmitEventReviewPreviewTest.php
@@ -1,14 +1,29 @@
 <?php
 
+use Livewire\Livewire;
+
 it('shows a submission preview section on submit event page', function () {
-    $this->get('/submit-event')
+    $this->get(route('submit-event.create'))
         ->assertSuccessful()
         ->assertSee(__('Pratonton Penghantaran'))
-        ->assertSee(__('Semak ringkasan ini sebelum anda menghantar.'));
+        ->assertSee(__('Semak ringkasan ini sebelum anda menghantar.'))
+        ->assertSee(__('Seterusnya'));
+});
+
+it('hides the next action when the review step rerenders', function () {
+    $reviewStepId = 'form.semak-sebelum-hantar::data::wizard-step';
+
+    Livewire::withQueryParams(['step' => $reviewStepId])
+        ->test('pages.submit-event.create')
+        ->assertSet('wizardStep', $reviewStepId)
+        ->call('$refresh')
+        ->assertSee(__('Sebelum'))
+        ->assertSee(__('Hantar Majlis untuk Semakan'))
+        ->assertDontSee(__('Seterusnya'));
 });
 
 it('loads app-level filament helper scripts on submit event page', function () {
-    $this->get('/submit-event')
+    $this->get(route('submit-event.create'))
         ->assertSuccessful()
         ->assertSee('close-on-select.js', false)
         ->assertSee('user-timezone.js', false);
