409a605ec5ab587d6aefe4352c80a6d695d732a9
Saiffil Fariz <saiffil@masyuk.com>
2026-04-22 09:09:15 +0800

    wip

--- config/filament-signals.php
+++ config/filament-signals.php
@@ -6,7 +6,7 @@ return array_replace_recursive(
     require base_path('vendor/aiarmada/filament-signals/config/filament-signals.php'),
     [
         'features' => [
-            'dashboard' => false,
+            'dashboard' => true,
         ],
     ],
 );
